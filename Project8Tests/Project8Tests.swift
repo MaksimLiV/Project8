e49da94f71518f4c055025482b11c9548f646c3a
Maksim Li <maximlee.ml@gmail.com>
2024-11-03 12:38:02 +0200

    Initial Commit

---
+++ Project8Tests/Project8Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project8Tests.swift
+//  Project8Tests
+//
+//  Created by Maksim Li on 03/11/2024.
+//
+
+import Testing
+@testable import Project8
+
+struct Project8Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
